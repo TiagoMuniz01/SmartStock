a77a8316d4f54a4b3277a854c6ab6e5f4211bda0
Tiago Pimentel Muniz <131329736+TiagoMuniz01@users.noreply.github.com>
2025-05-03 16:53:36 -0300

    Configurações - Configurações iniciais
    Configurando as tecnologias e dependências iniciais do projeto. Vite iniciado e configurado, tailwind iniciado, diretorios organizados.

---
+++ SmartStock/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+/* 
+import reactLogo from './assets/react.svg'
+<img src={reactLogo} className="logo react" alt="React logo" />
+
+*/
+function App() {
+
+  return (
+    <>
+      <h1 className="text-5xl font-bold">SmartStock</h1>
+    </>
+  )
+}
+
+export default App

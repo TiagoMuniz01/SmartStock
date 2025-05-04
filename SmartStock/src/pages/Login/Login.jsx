dbb546ab5b30fd1e035ee3b61d17cd1fbbb01ff9
Tiago Pimentel Muniz <131329736+TiagoMuniz01@users.noreply.github.com>
2025-05-04 02:01:14 -0300

    Gradiente criado
    Gradiente da tela de login criado

--- SmartStock/src/pages/Login/Login.jsx
+++ SmartStock/src/pages/Login/Login.jsx
@@ -4,7 +4,7 @@ import style from './Login.module.css'
 
 const Login = () => {
     return(
-        <div className="h-screen bg-linear-150 from-primaria from-80% to-secundaria">
+        <div className="h-screen bg-linear-150 from-primaria-70 from-50% via-primaria-50 via-80% to-secundaria to-100%">
 
         </div>
     )
